775e560a1f203cb6213876d7d2505c6defdb56af
lechuan01 <lechuan2001@gmail.com>
2021-11-19 13:26:46 +0000

    Update receive.js

--- src/routes/receive.js
+++ src/routes/receive.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const acceptcontroller = require('../controllers/AcceptController');
 
 router.use('/accept',acceptcontroller.index);
-router.use('/list',acceptcontroller.get_list);
-router.use('/detail:id',acceptcontroller.get_detail);
 router.get('/', (req, res, next) => {
     res.render('volunteer/receive');
 });
 /*
+router.use('/detail:id',acceptcontroller.get_detail);
+router.use('/list',acceptcontroller.get_list);
 router.get('/accept', (req, res, next) => {
     res.render('volunteer/accept');
 });*/
