b35bf3417259beaae05bcd8379f05924143697db
thuyngan1501 <90319390+thuyngan1501@users.noreply.github.com>
2021-11-26 04:15:48 +0700

    TN update
    TN update

--- src/controllers/checkoutController.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-const db = require('../config/db/DBconnection');
-
-exports.showCheck = (req, res, next) => {
-    // const cart = Cart.getCart();
-    // res.render('cart', { cart });
-    res.render('checkout');
-}
