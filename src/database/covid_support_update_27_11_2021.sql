f09757a489a5718b899e52417819e5abacdc350c
Bao Nguyen Thien <nguyenthienbao0402@gmail.com>
2021-11-27 21:09:29 +0700

    update database

--- src/database/covid_support_update_27_11_2021.sql
+++ src/database/covid_support_update_27_11_2021.sql
@@ -44,7 +44,7 @@ CREATE TABLE `account` (
 
 LOCK TABLES `account` WRITE;
 /*!40000 ALTER TABLE `account` DISABLE KEYS */;
-INSERT INTO `account` VALUES ('31d93240-4e0a-11ec-ab19-57d2fcbeb0ed','Lê Minh','Chuẩn','2001-01-01','1234567890','$2b$10$nytEFocvK7x2LqmVWKiC6OgQ8vhIY/ufT9AeinZWSpYPryx3C.Ase','2021-11-25 23:10:23','img_7229_1509dab752bf4f27a15ac210122b92df.jpg','V'),('869d0740-49bc-11ec-ad87-41acf4791c20','Nguyễn Hoài','Nam','2001-03-01','123456789','$2b$10$C0pkE.A6sNGwHs155PGrkOfbByJLdXaVNRJw7Ydi0.auuKzUIefkq','2021-11-20 11:44:20','360_F_222851624_jfoMGbJxwRi5AWGdPgXKSABMnzCQo9RN.jpg','V'),('904a3a50-4d32-11ec-9202-bb96320a7590','Phạm Thị Thúy','Ngân','2001-04-02','012345678','$2b$10$tTXB8D3LLIFawf4BoocgmubeM1QVEmTTddt/ngL/H/DpDXsSL7Nc.','2021-11-24 21:26:50','women-face-portrait-gray-eyes-wallpaper-preview.jpg','C'),('9fbe9870-4e93-11ec-bb3c-4b9e63704a49','NGUYỄN THIÊN','BẢO','2001-02-04','0936010095','$2b$10$.z8hYLUgHr4OU9vx1PhUReRYR/7J8CUZV0Ub6ANi10DZQULGrSyXG','2021-11-26 15:34:08','avatar-default.jpg','A'),('a2a142f0-4825-11ec-a190-53e797f7b5e3','Lê Thị Như','Ý','1999-08-01','0936010094','$2b$10$ckO9GLPczh1/BvkuAvigqeKI45zd1Fbi4flYXz9wMHtkCq3eFAkt2','2021-11-18 11:11:42','CODERED_B1_portrait_photography-P4a_438x447.jpg.img.jpg','C');
+INSERT INTO `account` VALUES ('31d93240-4e0a-11ec-ab19-57d2fcbeb0ed','Lê Minh','Chuẩn','2001-01-01','1234567890','$2b$10$nytEFocvK7x2LqmVWKiC6OgQ8vhIY/ufT9AeinZWSpYPryx3C.Ase','2021-11-25 23:10:23','img_7229_1509dab752bf4f27a15ac210122b92df.jpg','V'),('869d0740-49bc-11ec-ad87-41acf4791c20','Nguyễn Hoài','Nam','2001-03-01','123456789','$2b$10$C0pkE.A6sNGwHs155PGrkOfbByJLdXaVNRJw7Ydi0.auuKzUIefkq','2021-11-20 11:44:20','360_F_222851624_jfoMGbJxwRi5AWGdPgXKSABMnzCQo9RN.jpg','V'),('904a3a50-4d32-11ec-9202-bb96320a7590','Phạm Thị Thúy','Ngân','2001-03-31','012345678','$2b$10$tTXB8D3LLIFawf4BoocgmubeM1QVEmTTddt/ngL/H/DpDXsSL7Nc.','2021-11-24 21:26:50','women-face-portrait-gray-eyes-wallpaper-preview.jpg','C'),('9fbe9870-4e93-11ec-bb3c-4b9e63704a49','NGUYỄN THIÊN','BẢO','2001-02-04','0936010095','$2b$10$.z8hYLUgHr4OU9vx1PhUReRYR/7J8CUZV0Ub6ANi10DZQULGrSyXG','2021-11-26 15:34:08','avatar-default.jpg','A'),('a2a142f0-4825-11ec-a190-53e797f7b5e3','Lê Thị Như','Ý','2001-07-31','0936010094','$2b$10$ckO9GLPczh1/BvkuAvigqeKI45zd1Fbi4flYXz9wMHtkCq3eFAkt2','2021-11-18 11:11:42','cac-mon-an-chinh-cua-nguoi-chau-au-01-min.jpg','C'),('d875dec0-4f89-11ec-8589-9be3d32681c6','Phạm Thị Thúy','Nam','2000-02-09','0123789456','$2b$10$WaXFmHctbetjEecJQnR9du2Ph8ECsm/65.9HCLzlIp/zVyacstD5u','2021-11-27 20:56:40','bacon-rings-103588-1.jpeg','V');
 /*!40000 ALTER TABLE `account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -69,7 +69,7 @@ CREATE TABLE `account_address` (
 
 LOCK TABLES `account_address` WRITE;
 /*!40000 ALTER TABLE `account_address` DISABLE KEYS */;
-INSERT INTO `account_address` VALUES ('869d0740-49bc-11ec-ad87-41acf4791c20','1, đường số 5, Phường 12, Quận 3, Thành phố Hồ Chí Minh'),('a2a142f0-4825-11ec-a190-53e797f7b5e3','12, đường số 4, Phường Bình Trị Đông, Quận Bình Tân, Thành phố Hồ Chí Minh'),('a2a142f0-4825-11ec-a190-53e797f7b5e3','245, đường Đinh Tiên Hoàng, Phường 03, Quận 8, Thành phố Hồ Chí Minh');
+INSERT INTO `account_address` VALUES ('869d0740-49bc-11ec-ad87-41acf4791c20','1, đường số 5, Phường 12, Quận 3, Thành phố Hồ Chí Minh'),('904a3a50-4d32-11ec-9202-bb96320a7590','12, đường 3, Phường Võ Thị Sáu, Quận 3, Thành phố Hồ Chí Minh'),('a2a142f0-4825-11ec-a190-53e797f7b5e3','12, đường số 4, Phường Bình Trị Đông, Quận Bình Tân, Thành phố Hồ Chí Minh'),('a2a142f0-4825-11ec-a190-53e797f7b5e3','245, đường Đinh Tiên Hoàng, Phường 03, Quận 8, Thành phố Hồ Chí Minh');
 /*!40000 ALTER TABLE `account_address` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -175,7 +175,7 @@ CREATE TABLE `order` (
   KEY `volunteer_id` (`volunteer_id`),
   CONSTRAINT `order_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`account_id`),
   CONSTRAINT `order_ibfk_2` FOREIGN KEY (`volunteer_id`) REFERENCES `volunteer` (`account_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -184,7 +184,7 @@ CREATE TABLE `order` (
 
 LOCK TABLES `order` WRITE;
 /*!40000 ALTER TABLE `order` DISABLE KEYS */;
-INSERT INTO `order` VALUES (10,'2021-11-19 22:37:06','quận Bình Tân, Hồ Chí Minh','Chờ giao hàng','869d0740-49bc-11ec-ad87-41acf4791c20','2021-11-20 12:03:37',0,'a2a142f0-4825-11ec-a190-53e797f7b5e3','quận Bình Tân'),(12,'2021-11-26 11:28:50','36, đường 3C, phường An Lạc, quận Bình Tân','Chờ giao hàng','31d93240-4e0a-11ec-ab19-57d2fcbeb0ed','2021-11-26 11:59:10',0,'a2a142f0-4825-11ec-a190-53e797f7b5e3','quận Bình Tân'),(13,'2021-11-26 12:08:05','12, đường Đa Kao, quận 1','Chờ giao hàng','31d93240-4e0a-11ec-ab19-57d2fcbeb0ed','2021-11-26 15:27:29',0,'904a3a50-4d32-11ec-9202-bb96320a7590','quận 1'),(14,'2021-11-26 12:19:20','12, đường 45, thành phố Thủ Đức','Chờ tiếp nhận',NULL,NULL,0,'904a3a50-4d32-11ec-9202-bb96320a7590','thành phố Thủ Đức'),(15,'2021-11-26 12:20:44','456, đường Hồng Bàng, quận 5','Chờ tiếp nhận',NULL,NULL,0,'904a3a50-4d32-11ec-9202-bb96320a7590','quận 5'),(16,'2021-11-27 12:31:59','245, đường Đinh Tiên Hoàng, Phường 03, Quận 8, Thành phố Hồ Chí Minh','Chờ tiếp nhận',NULL,NULL,0,'a2a142f0-4825-11ec-a190-53e797f7b5e3','Quận 8');
+INSERT INTO `order` VALUES (10,'2021-11-19 22:37:06','quận Bình Tân, Hồ Chí Minh','Đã hoàn thành','869d0740-49bc-11ec-ad87-41acf4791c20','2021-11-20 12:03:37',120000,'a2a142f0-4825-11ec-a190-53e797f7b5e3','quận Bình Tân'),(12,'2021-11-26 11:28:50','36, đường 3C, phường An Lạc, quận Bình Tân','Chờ giao hàng','31d93240-4e0a-11ec-ab19-57d2fcbeb0ed','2021-11-26 11:59:10',0,'a2a142f0-4825-11ec-a190-53e797f7b5e3','quận Bình Tân'),(13,'2021-11-26 12:08:05','12, đường Đa Kao, quận 1','Đã hoàn thành','31d93240-4e0a-11ec-ab19-57d2fcbeb0ed','2021-11-26 15:27:29',560000,'904a3a50-4d32-11ec-9202-bb96320a7590','quận 1'),(14,'2021-11-26 12:19:20','12, đường 45, thành phố Thủ Đức','Chờ giao hàng','31d93240-4e0a-11ec-ab19-57d2fcbeb0ed','2021-11-27 18:54:36',0,'904a3a50-4d32-11ec-9202-bb96320a7590','thành phố Thủ Đức'),(15,'2021-11-26 12:20:44','456, đường Hồng Bàng, quận 5','Đã hoàn thành','31d93240-4e0a-11ec-ab19-57d2fcbeb0ed','2021-11-27 18:53:09',45000,'904a3a50-4d32-11ec-9202-bb96320a7590','quận 5'),(16,'2021-11-27 12:31:59','245, đường Đinh Tiên Hoàng, Phường 03, Quận 8, Thành phố Hồ Chí Minh','Chờ tiếp nhận',NULL,NULL,0,'a2a142f0-4825-11ec-a190-53e797f7b5e3','Quận 8'),(17,'2021-11-27 20:49:08','245, đường Đinh Tiên Hoàng, Phường 03, Quận 8, Thành phố Hồ Chí Minh','Chờ giao hàng','31d93240-4e0a-11ec-ab19-57d2fcbeb0ed','2021-11-27 20:49:54',0,'a2a142f0-4825-11ec-a190-53e797f7b5e3','Quận 8'),(18,'2021-11-27 21:02:28','12, đường số 4, Phường Bình Trị Đông, Quận Bình Tân, Thành phố Hồ Chí Minh','Chờ tiếp nhận',NULL,NULL,0,'a2a142f0-4825-11ec-a190-53e797f7b5e3','Quận Bình Tân'),(19,'2021-11-27 21:02:42','12, đường số 4, Phường Bình Trị Đông, Quận Bình Tân, Thành phố Hồ Chí Minh','Chờ tiếp nhận',NULL,NULL,0,'a2a142f0-4825-11ec-a190-53e797f7b5e3','Quận Bình Tân'),(20,'2021-11-27 21:02:59','245, đường Đinh Tiên Hoàng, Phường 03, Quận 8, Thành phố Hồ Chí Minh','Chờ tiếp nhận',NULL,NULL,0,'a2a142f0-4825-11ec-a190-53e797f7b5e3','Quận 8'),(21,'2021-11-27 21:03:37','245, đường Đinh Tiên Hoàng, Phường 03, Quận 8, Thành phố Hồ Chí Minh','Chờ tiếp nhận',NULL,NULL,0,'a2a142f0-4825-11ec-a190-53e797f7b5e3','Quận 8');
 /*!40000 ALTER TABLE `order` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -211,7 +211,7 @@ CREATE TABLE `order_item` (
 
 LOCK TABLES `order_item` WRITE;
 /*!40000 ALTER TABLE `order_item` DISABLE KEYS */;
-INSERT INTO `order_item` VALUES (10,'Cá hú','2 con',''),(10,'Sữa tươi','3 hộp','loại 2 Lít'),(10,'Tiêu','2kg','Con'),(12,'bếp gas','2 cái',''),(12,'lò vi sóng','2 cái','panasonic'),(13,'Bánh gạo','2 bịt',''),(13,'Máy sấy tóc','2 cái',''),(13,'Tương ớt chinsu','10 chai','chai nhỏ'),(14,'Bắp cải trắng','3 bắp',''),(14,'Cá hú','2 con',''),(14,'Cafe Trung Nguyên sáng tạo 4','5 bịt',''),(15,'Keo dán giấy','4 chai',''),(16,'Mỳ Hảo Hảo','2 thùng','Tôm chua cay');
+INSERT INTO `order_item` VALUES (10,'Cá hú','2 con',''),(10,'Sữa tươi','3 hộp','loại 2 Lít'),(10,'Tiêu','2kg','Con'),(12,'bếp gas','2 cái',''),(12,'lò vi sóng','2 cái','panasonic'),(13,'Bánh gạo','2 bịt',''),(13,'Máy sấy tóc','2 cái',''),(13,'Tương ớt chinsu','10 chai','chai nhỏ'),(14,'Bắp cải trắng','3 bắp',''),(14,'Cá hú','2 con',''),(14,'Cafe Trung Nguyên sáng tạo 4','5 bịt',''),(15,'Keo dán giấy','4 chai',''),(16,'Mỳ Hảo Hảo','2 thùng','Tôm chua cay'),(17,'Bò kho','3 tô','Thêm bánh mì'),(18,'Mỳ Omachi','1 thùng',''),(18,'Mỳ Ý','2 gói','Cảm ơn'),(19,'Mỳ Ý','4 thùng',''),(20,'Nước suối','2 thùng','Lavie'),(21,'Nước ngọt','2 ','Cocacola');
 /*!40000 ALTER TABLE `order_item` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -237,7 +237,7 @@ CREATE TABLE `volunteer` (
 
 LOCK TABLES `volunteer` WRITE;
 /*!40000 ALTER TABLE `volunteer` DISABLE KEYS */;
-INSERT INTO `volunteer` VALUES ('lechuan@gmail.com','T','31d93240-4e0a-11ec-ab19-57d2fcbeb0ed'),('namnguyen@gmail.com','F','869d0740-49bc-11ec-ad87-41acf4791c20');
+INSERT INTO `volunteer` VALUES ('lechuan@gmail.com','T','31d93240-4e0a-11ec-ab19-57d2fcbeb0ed'),('namnguyen@gmail.com','T','869d0740-49bc-11ec-ad87-41acf4791c20'),('thuynam@gmail.com','T','d875dec0-4f89-11ec-8589-9be3d32681c6');
 /*!40000 ALTER TABLE `volunteer` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -250,4 +250,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-27 13:46:12
+-- Dump completed on 2021-11-27 21:08:49
