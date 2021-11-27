3cd3d8427bdd79bcca3cbf54e39c4d8a39283f33
Bao Nguyen Thien <nguyenthienbao0402@gmail.com>
2021-11-27 22:26:41 +0700

    Enable user management

--- src/routes/receive.js
+++ src/routes/receive.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const receiveController = require('../controllers/ReceiveController');
 const acceptController = require('../controllers/AcceptController');
 const auth = require('../controllers/AuthenticateUser');
-router.get('/', receiveController.show);
+router.get('/', auth(['V']), receiveController.show);
 
-router.post('/confirm', receiveController.confirm);
+router.post('/confirm', auth(['V']), receiveController.confirm);
 
-router.get('/accept', acceptController.show);
+router.get('/accept', auth(['V']), acceptController.show);
 
-router.post('/accept', acceptController.confirmDoneOrder);
-router.post('/accept/details', acceptController.showDetail);
+router.post('/accept', auth(['V']), acceptController.confirmDoneOrder);
+
+router.post('/accept/check-list', auth(['V']), acceptController.checkList);
+
+router.post('/accept/details', auth(['V']), acceptController.showDetail);
 
 
 
